3124c7d0bab377e7c6723b1209827c9788a8394a
Lê Thành Đạt <lacthuyit@gmail.com>
2024-12-28 19:34:00 +0900

    Update api host

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a911dffe876a840e5baed439dbfd0656-407518445.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
